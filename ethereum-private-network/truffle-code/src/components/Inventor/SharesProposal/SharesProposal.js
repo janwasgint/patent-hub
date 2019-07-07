3d306a5990959028faa1f21c9b283f3d224146f5
Jan Wasgint <jan.wasgint@motius.de>
2019-07-07 16:48:59 +0200

    Fix string

--- ethereum-private-network/truffle-code/src/components/Inventor/SharesProposal/SharesProposal.js
+++ ethereum-private-network/truffle-code/src/components/Inventor/SharesProposal/SharesProposal.js
@@ -73,7 +73,7 @@ class SharesProposal extends Component {
           {!this.props.showAcceptProposal && (
             <div>
               <div className="alert alert-success" role="alert">
-                All inventors accepted the shares proposal
+                All inventors accepted the shares proposal.
               </div>
             </div>
           )}
